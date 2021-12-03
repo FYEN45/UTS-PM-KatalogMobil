a1d182050311fdece2bfb5b437c05f2b621a3050
FYEN45 <ferry.ahogun@gmail.com>
2021-12-03 19:58:57 +0700

    update readme n php

---
+++ PHP_XAMPP_MYSQL/connect.php
@@ -0,0 +1,32 @@
+<?php
+    error_reporting(0);
+    require_once __DIR__. '/config.php';
+    
+    class DB_CONNECT {
+        public $con;
+
+        function __construct()
+        {
+            $this->connect_DB();
+        }
+
+        function __destruct()
+        {
+            $this->close_DB();
+        }
+
+        function connect_DB(){
+            $this->con = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE);
+            if(mysqli_connect_errno()){
+                printf("Koneksi Gagal! \n", mysqli_connect_error());
+                exit();
+            }
+            printf("helo");
+            return $this->con;
+        }
+
+        function close_DB(){
+            mysqli_close($this->con);
+        }
+    }
+?>
--- README.md
+++ README.md
@@ -5,6 +5,17 @@
 - Reynaldo Krisno 32190097
 - Ferry Gunawan 32190098
 
+# IMPORTANT #
+JANGAN LUPA NYALAIN XAMPP!
+JANGAN LUPA PINDAHIN FOLDER PHP KE XAMPP!
+JANGAN LUPA IMPORT DATABASE KE MYSQL!
+
+di dalam folder PHP_XAMPP_MYSQL (KatalogKelompok/PHP_XAMPP_MYSQL) ada folder "CarCatalog".
+"CarCatalog" dimasukkin ke folder "htdocs" nya XAMPP.
+
+Database ada di dalam folder PHP_XAMPP_MYSQL (KatalogKelompok/PHP_XAMPP_MYSQL).
+##############################
+
 # What to do?
 - Gridlist / Gridview gambar mobil.
 - Halaman Deskripsi mobil
