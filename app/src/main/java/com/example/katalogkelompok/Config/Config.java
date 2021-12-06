49b69441a6cbe343404d58d12901574df5fb97c9
FYEN45 <ferry.ahogun@gmail.com>
2021-12-06 16:50:33 +0700

    Update Config.java

--- app/src/main/java/com/example/katalogkelompok/Config/Config.java
+++ app/src/main/java/com/example/katalogkelompok/Config/Config.java
@@ -2,9 +2,10 @@ package com.example.katalogkelompok.Config;
 
 public interface Config {
     //TODO : Ganti IP
-    public String alamatIP = "192.168.1.7";
+    public String alamatIP = "192.168.1.13";
 
     public String userLoginVerification = "http://"+alamatIP+"/carcatalog/user_login_verification.php/";
     public String userRegister = "http://"+alamatIP+"/carcatalog/user_register.php/";
     public String requestUserList = "http://"+alamatIP+"/carcatalog/request_user_list.php/";
+    public String requestUpdateData = "http://"+alamatIP+"/carcatalog/request_user_update.php/";
 }
